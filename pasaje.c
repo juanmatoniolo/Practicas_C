e24639fd23647fa8268f34f8deb9c67f3cac387a
JunmaToniolo <juanmatoniolo2@gmail.com>
2022-08-30 11:04:06 -0300

    1

---
+++ pasaje.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+// Funciones simples
+int cubo(int);
+
+/*//Main funcion cubo
+int main()
+{
+    int x;
+
+    printf("Ingrese un numero para elevarlo al cubo:\n");
+    printf(">");
+    scanf("%d", &x);
+    printf("El numero que ingreso elevado al cubo es: %d", cubo(x));
+
+    return 0;
+}
+*/
+
+/* Bucle for para sacar el cubo de n numeros
+int main()
+{
+    int numero;
+    for (numero = 1; numero <= 5; numero++)
+        printf("El cubo de %d es igual a %d\n", numero, cubo(numero));
+    return 0;
+}
+*/
+
+
+int cubo(int x)
+{
+    return x * x * x;
+}
