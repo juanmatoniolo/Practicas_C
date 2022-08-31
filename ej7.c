08a1a64ecf86751afe7bac5d0300e9132dbfb1ed
JunmaToniolo <juanmatoniolo2@gmail.com>
2022-08-31 01:56:37 -0300

    Hasta ejercicio 8

---
+++ ej7.c
@@ -0,0 +1,72 @@
+// F2-2) Crear un programa que implemente las siguientes funciones para manipular un array de 10 enteros.           Las siguientes declaraciones de funciones deben utilizarse y definirse adecuadamente.     void cargar(int []);          //carga un array con valores suministrados por el usuario     void mostrar(int []);         //muestra un array     int pertenece(int [], int);   //determina si un entero está en el array
+
+#include <stdio.h>
+#include <stdlib.h>
+#define N 10
+
+void cargar(int[]);
+void mostar(int[]);
+int pertenece(int[], int);
+
+int main()
+{
+    int lista[N];
+    int buscar, op;
+
+    cargar(lista);
+    mostar(lista);
+    pertenece(lista, buscar);
+    
+
+    return 0;
+}
+
+void cargar(int list[])
+{
+    int i;
+
+    printf("Ingrese 10 numeros enteros:\n");
+    for (i = 0; i < N; i++)
+    {
+        printf("Ingrese el %d numeros enteros:\n", i + 1);
+        printf(">");
+        scanf("%d", &list[i]);
+    }
+}
+
+void mostar(int list[])
+{
+    int i;
+
+    for (i = 0; i < N; i++)
+    {
+        printf(" %d  de la lista es: %d \n", i + 1, list[i]);
+    }
+}
+
+int pertenece(int list[], int aux)
+{
+    int i;
+    int cont = 0;
+
+    printf("Ingrese un numero y le dire si esta en la lista: \n");
+    printf(">");
+    scanf("%d", &aux);
+
+    for (i = 0; i < N; i++)
+    {
+        if (aux == list[i])
+        {
+            cont++;
+        }
+    }
+
+    if (cont > 0)
+    {
+        printf("El numero %d se encuentra en la lista!", aux);
+    }
+    else
+    {
+        printf("El numero %d NO pertenece a la lista", aux);
+    }
+}
---
+++ ej8.c
@@ -0,0 +1,49 @@
+// F2-3) Dado un array de N enteros, crear una función para invertirlo y posteriormente mostrarlo por pantalla.
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define N 10
+int listaaleatoria(int[]);
+int vuelta(int[]);
+
+int main()
+{
+    int lista[N];
+
+    printf("Primero generaremos una lista al azar:\n");
+    listaaleatoria(lista);
+
+    printf("\n \nAhora daremos vuelta la lista:\n");
+    vuelta(lista);
+
+    return 0;
+}
+
+int vuelta(int list[])
+{
+    int i, j, aux;
+    for (i = 0, j = N - 1; i < j; i++, j--)
+    {
+        aux = list[i]; //Esta parte costo sacar
+        list[i] = list[j];//lo que hacemos es cambiar los numeros de i de j con aux 
+        list[j] = aux; // i lo cambiamos por N-1, (i[0] ahora pasa a ser i[N-1 || 9] ) 
+    }
+    for ( i = 0; i < N; i++)
+    {
+        printf("%d- %d\n", i+1, list[i]); //este for es para mostrar los numeros porque si lo hacemos arriba solo llegamos a mostrar 5 numeros al reves
+    }
+    
+}
+
+int listaaleatoria(int lista[])
+{
+    int i;
+    srand(time(NULL));
+    for (i = 0; i < N; i++)
+    {
+        lista[i] = rand() % 100 + 1;
+        printf("%d- %d \n", i + 1, lista[i]);
+    }
+}
