956b65b567b779db9082788b29b55d64165409b6
JunmaToniolo <juanmatoniolo2@gmail.com>
2022-08-31 16:13:21 -0300

    ej 10

---
+++ ej10.c
@@ -0,0 +1,69 @@
+// F3-1) Crear funciones que mediante arrays se emulen operaciones de conjuntos. -
+//- En primer lugar existirán dos arrays A y B de 10 elementos enteros comprendidos en el rango [0. 100].
+//- Debe crearse una función que cargue los arrays de forma aleatoria pero que no ingrese valores repetidos en cada array, es decir debemos controlar en esa función que cuando se propone un número aleatorio para almacenar en la posición i del array no esté dicho número en ninguna de las posiciones 0, 1, …, i-1.
+//- Luego de la carga de los arrays mostrarlos por pantalla.
+//- Crear una función que muestre por pantalla la intersección de estos “conjuntos”, es decir los elementos de A que estén presentes en B.
+// - Crear una función que muestre por pantalla la unión de estos “conjuntos”, es decir los elementos pertenecientes a A y B, sin repetir.
+// - Crear una función que muestre por pantalla la diferencia entre estos “conjuntos”, es decir los elementos pertenecientes a A no pertenecientes a B.
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#define N 10
+#define NUM 100
+
+int carga(int[]);
+int muestra(int[]);
+int pertenece(int[], int, int);
+
+int main()
+{
+    int A[N], B[N];
+    printf("Primero generamos una lista y luego la imprimimos por pantalla:\n\n");
+    carga(A);
+    carga(B);
+
+    printf("Mostraremos la lista A:\n");
+    muestra(A);
+
+    printf("\n\nMostramos la lista B:\n");
+    muestra(B);
+
+    return 0;
+}
+
+int pertenece(int V[], int valor, int pos)
+{
+    int i;
+    for (i = 0; i < pos; i++)
+    {
+        if (V[i] == valor)
+            return 1;
+    }
+    return 0;
+}
+
+int carga(int list[])
+{
+    int i;
+
+    for (i = 0; i < N; i++)
+    {
+        do
+        {
+            list[i] = rand() % NUM + 1;
+        } while (pertenece(list, list[i], i));
+    }   //Pertenece toma la lista entera, el valor de lista en i y dice si i esta o no
+        // pertenece devuelve 1 si esta 0 si no esta, i!=0
+}
+
+int muestra(int lista[])
+{
+    int i;
+    printf("{ ");
+    for (i = 0; i < N; i++)
+    {
+        printf("%d ", lista[i]);
+    }
+    printf("}\n ");
+}
---
+++ eje11.c
@@ -0,0 +1,7 @@
+//F3-2) Calculo total de una compra. Crear un programa que permita tener almacenado el precio unitario de 5 productos diferentes. El usuario indica cuantos productos quiere comprar de cada tipo, para indicar ninguno ingresará 0, por supuesto evitar valores negativos. Si el usuario compra más de 10 productos de un mismo tipo, se debe aplicar un 8% de descuento sobre el costo total de los productos de ese tipo en particular. Si el usuario supera un monto máximo (especificarlo con una macro), se le debe aplicar un descuento del 15% al costo total de toda la compra. Crear funciones para: almacenar el precio de los 5 productos, permitir al usuario ingresar cuantos productos de cada tipo desea adquirir. Mostrar por pantalla los descuentos que obtuvo (si aplica alguno) y el costo total de la compra.
+
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
